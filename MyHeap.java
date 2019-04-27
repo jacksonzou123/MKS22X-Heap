736770f5a5c8177961d6602d7e88c100e6091eda
Jackson Zou <jzou00@stuy.edu>
2019-04-27 00:18:36 -0400

    started writing push down

--- MyHeap.java
+++ MyHeap.java
@@ -1,6 +1,10 @@
-public class MyHeap{
-  private static void pushDown(int[] data,int size,int index) {
+//parent node is (n-1)/2
+//child nodes are 2n+1 or 2n+2
 
+public class MyHeap{
+  private static void pushDown(int[] data, int size, int index) {
+    int num = data[index];
+    if (2 * index + 2 < size )
   }
 
   private static void pushUp(int[] data,int index) {
