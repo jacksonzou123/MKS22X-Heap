23859e320a4671ca5b4f75569ddb171511b05ef3
Jackson Zou <jzou00@stuy.edu>
2019-04-28 19:48:41 -0400

    ok

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Driver{
   public static void main(String[] args) {
-    int[] ary = new int[] {5,7,8,10,9,11,15,19,17,4,8};
+    int[] ary = new int[] {5,7,8,10,9,11,1,3,456,33,88,12,135,6,419,17,4,8};
     System.out.println(Arrays.toString(ary));
     MyHeap.heapify(ary);
     System.out.println(Arrays.toString(ary));
