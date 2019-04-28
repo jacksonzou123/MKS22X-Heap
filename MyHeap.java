fe5fe987ab7a2a55d30387c870a2cafcbb44e445
Jackson Zou <jzou00@stuy.edu>
2019-04-28 19:44:19 -0400

    wrote heapify

--- MyHeap.java
+++ MyHeap.java
@@ -2,16 +2,16 @@
 //child nodes are 2n+1 or 2n+2
 
 public class MyHeap{
-  private static void pushDown(int[] data, int size, int index) {
+  public static void pushDown(int[] data, int size, int index) {
     int num = data[index];
     if (2 * index + 2 < size) {
-      if (num < data[2 * index + 1]) {
+      if (num < data[2 * index + 1] && data[2 * index + 1] > data[2 * index + 2]) {
         data[index] = data[2 * index + 1];
         data[2 * index + 1] = num;
         num = data[2 * index + 1];
         pushDown(data, size, 2 * index + 1);
       }
-      if (num < data[2 * index + 2]) {
+      else if (num < data[2 * index + 2]) {
         data[index] = data[2 * index + 2];
         data[2 * index + 2] = num;
         num = data[2 * index + 2];
@@ -33,7 +33,9 @@ public class MyHeap{
   }
 
   public static void heapify(int[] data) {
-
+    for (int i = data.length / 2; i >= 0; i--) {
+      pushDown(data, data.length, i);
+    }
   }
 
   public static void heapsort(int[] data) {
