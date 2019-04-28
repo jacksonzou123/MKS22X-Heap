16483b7c6fc3a437ff1f37554f5d32866fd6a36b
Jackson Zou <jzou00@stuy.edu>
2019-04-28 18:17:29 -0400

    wrote pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -4,7 +4,25 @@
 public class MyHeap{
   private static void pushDown(int[] data, int size, int index) {
     int num = data[index];
-    if (2 * index + 2 < size )
+    if (2 * index + 2 < size) {
+      if (num < data[2 * index + 1]) {
+        data[index] = data[2 * index + 1];
+        data[2 * index + 1] = num;
+        num = data[2 * index + 1];
+      }
+      if (num < data[2 * index + 2]) {
+        data[index] = data[2 * index + 2];
+        data[2 * index + 2] = num;
+        num = data[2 * index + 2];
+      }
+    }
+    else if (2 * index + 1 < size) {
+      if (num < data[2 * index + 1]) {
+        data[index] = data[2 * index + 1];
+        data[2 * index + 1] = num;
+        num = data[2 * index + 1];
+      }
+    }
   }
 
   private static void pushUp(int[] data,int index) {
